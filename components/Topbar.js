fd2281225f2af7d09f5e42e55407b38d60086bd4
Simon M <marcotte.s2010@gmail.com>
2024-04-29 23:06:47 -0700

    split topbar component

---
+++ components/Topbar.js
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react"
+import { Avatar } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
+
+export default function Topbar({email}){
+
+    return (
+        <Flex bg={"gray.100"} h={"81px"} w={"100%"}align={"center"} p={5}>
+            <Avatar src="" marginEnd={3}/>
+            <Heading size={"lg"}>{email}</Heading>
+        </Flex>
+    )
+}
